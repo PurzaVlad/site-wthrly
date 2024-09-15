0f7f5e6d05a3ee7bf3b11679f374d5563f30a0bf
Purza Vlad <63791382+PurzaVlad@users.noreply.github.com>
2024-09-15 18:01:33 +0300

    Update settings.py

--- wthrly/settings.py
+++ wthrly/settings.py
@@ -128,8 +128,6 @@ STATICFILES = [
     "wthr.jpg"
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
