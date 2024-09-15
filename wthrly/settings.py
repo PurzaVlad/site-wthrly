b54adeced266624c9a2b3eb8076e88c128ac29b7
Purza Vlad <63791382+PurzaVlad@users.noreply.github.com>
2024-09-15 17:55:13 +0300

    Update settings.py

--- wthrly/settings.py
+++ wthrly/settings.py
@@ -115,17 +115,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = '/static/'
 
-STATICFILES = [
-    "styles.css",
-    "jsindex.js",
-    "jslocalitate.js",
-    "jsaction.js",
-    "jos2.svg",
-    "stg2.svg",
-    "wthr.jpg"
-]
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
